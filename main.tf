b6e2bf5c6c5a102691df1fe14867ba77b4947768
MaheshGit5096 <131481829+MaheshGit5096@users.noreply.github.com>
2024-01-26 15:00:13 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-south-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-039e1f129f345d75f"  # Replace with the desired AMI ID
+  instance_type = "t2.micro"                # Replace with the desired instance type
+  key_name      = "test.ppk"              # Replace with your key pair name
+
+  tags = {
+    Name = "NodeJS"               # Replace with your desired instance name
+  }
+}
+
+output "result" {
+value = [aws_instance.demo.public_ip]
