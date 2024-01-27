9371b9e1259e6829043f7ad320e7c952b8fe9949
MaheshGit5096 <131481829+MaheshGit5096@users.noreply.github.com>
2024-01-27 20:03:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "demo" {
   ami           = "ami-039e1f129f345d75f"  # Replace with the desired AMI ID
   instance_type = "t2.micro"                # Replace with the desired instance type
-  key_name      = "test.ppk"              # Replace with your key pair name
+  key_name      = "test"              # Replace with your key pair name
 
   tags = {
     Name = "NodeJS"               # Replace with your desired instance name
